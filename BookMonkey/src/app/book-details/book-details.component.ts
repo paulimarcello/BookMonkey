a4ac219a0a669521108bb64d2676093c82887697
Marcello <marcello@blade.fritz.box>
2020-01-09 15:40:36 +0100

    refactoring

--- BookMonkey/src/app/book-details/book-details.component.ts
+++ BookMonkey/src/app/book-details/book-details.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Book} from '../shared/book';
 import {ActivatedRoute, Router} from '@angular/router';
 import {BookStoreService} from '../shared/book-store.service';
+import { map, concatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'bm-book-details',
@@ -12,26 +13,19 @@ export class BookDetailsComponent implements OnInit {
   book: Book;
 
   constructor(
-    private route: ActivatedRoute,
+    private route$: ActivatedRoute,
     private router: Router,
     private bookStoreService: BookStoreService
   ) {
   }
 
   ngOnInit() {
-    this.isbnAusRouteAuslesen(this.route,
-      (isbn) => this.ladeBuch(isbn,
-        (book) => this.book = book));
-  }
-
-  private isbnAusRouteAuslesen(route: ActivatedRoute, callback: (isbn: string) => void): void {
-    route.paramMap.subscribe(paramMap => callback(paramMap.get('isbn')));
-  }
-
-  private ladeBuch(isbn: string, callback: (book: Book) => void): void {
-    this.bookStoreService
-      .getSingle(isbn)
-      .subscribe(callback);
+    this.route$.paramMap
+    .pipe(
+      map(paramMap => paramMap.get('isbn')),
+      concatMap(isbn => this.bookStoreService.getSingle(isbn))
+    )
+    .subscribe(book => this.book = book);
   }
 
   getRating(rating: number) {
